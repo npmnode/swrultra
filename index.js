ab0eef56ed85a7e97773d5c13cf7f84e78e25b95
red crowntail <Nglnodejs42@outlook.com>
2022-09-26 18:16:09 -0500

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const app = require('express')();
-
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 app.get('/', function(req, res){
-    res.send('hi');
+    res.send(`/xhr?u={url}&force={string}
+    <\br>
+    `);
+});
+app.get('/xhr', function(req, res){
+q=req.query;
+var xhttp = new XMLHttpRequest();
+xhttp.
+xhttp.open('GET', !!q.u?q.u:'https://swru.adabtable.app/');
 });
 
 app.listen(process.env.PORT||3000, console.log);
